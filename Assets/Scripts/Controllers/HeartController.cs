97f1809c310e3290ea6bb868df74481f1be5e74f
Jean Robert <jradesenv@gmail.com>
2018-07-08 18:15:01 -0300

    hearth system

---
+++ Assets/Scripts/Controllers/HeartController.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HeartController : MonoBehaviour {
+
+    public int startHearts = 3;
+    public int curHealth;
+    public Image[] healthImages;
+    public Sprite[] healthSprites;
+
+    private int maxHeartAmount = 10;
+    private int maxHealth;
+    private int healthPerHeart = 2;
+
+	// Use this for initialization
+	void Start () {
+        curHealth = startHearts * healthPerHeart;
+        maxHealth = maxHeartAmount * healthPerHeart;
+        CheckHealthAmount();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void CheckHealthAmount()
+    {
+        for (int i = 0; i < maxHeartAmount; i++)
+        {
+            if (startHearts <= i)
+            {
+                healthImages[i].enabled = false;
+            } else
+            {
+                healthImages[i].enabled = true;
+            }
+        }
+        UpdateHearts();
+    }
+
+    void UpdateHearts()
+    {
+        bool empty = false;
+        int i = 0;
+
+        foreach (Image image in healthImages)
+        {
+            if (empty)
+            {
+                image.sprite = healthSprites[0];
+            } else
+            {
+                i++;
+                if (curHealth >= i * healthPerHeart)
+                {
+                    image.sprite = healthSprites[healthSprites.Length - 1];
+                } else
+                {
+                    int currentHeartHealth = (int)(healthPerHeart - (healthPerHeart * i - curHealth));
+                    int healthPerImage = healthPerHeart / (healthSprites.Length - 1);
+                    int imageIndex = currentHeartHealth / healthPerImage;
+
+                    image.sprite = healthSprites[imageIndex];
+                    empty = true;
+                }
+            }
+        }
+    }
+
+    public void TakeDamage(int amount)
+    {
+        curHealth += amount;
+        curHealth = Mathf.Clamp(curHealth, 0, startHearts * healthPerHeart);
+        UpdateHearts();
+    }
+
+    public void AddHeartContainer()
+    {
+        startHearts++;
+        startHearts = Mathf.Clamp(startHearts, 0, maxHeartAmount);
+
+        //full heal
+        curHealth = startHearts * healthPerHeart;
+        maxHealth = maxHeartAmount * healthPerHeart;
+
+        CheckHealthAmount();
+    }
+
+
+}
---
+++ Assets/Scripts/Controllers/PlayerController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    [SerializeField]
+    private float vel;
+    private Vector2 direcao;
+    private Animator anim;
+	// Use this for initialization
+	void Start () {
+        anim = transform.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        InputPersonagem();
+        transform.Translate(direcao * vel * Time.deltaTime);
+
+        Animacao(direcao);
+	}
+
+    void InputPersonagem()
+    {
+        direcao = Vector2.zero;
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            direcao += Vector2.up;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            direcao += Vector2.down;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            direcao += Vector2.left;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            direcao += Vector2.right;
+        }
+    }
+
+    void Animacao(Vector2 dir)
+    {
+        if (direcao.x != 0 || direcao.y != 0)
+        {
+            anim.SetLayerWeight(1, 1);
+        } else
+        {
+            anim.SetLayerWeight(1, 0);
+        }
+        
+
+        anim.SetFloat("x", dir.x);
+        anim.SetFloat("y", dir.y);
+    }
+}
